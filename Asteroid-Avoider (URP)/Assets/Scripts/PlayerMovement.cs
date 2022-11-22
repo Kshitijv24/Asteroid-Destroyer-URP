b8359a6fa0f37fa984d346fe256809a48f3909b8
Kshitijv24 <kshitijv24@gmail.com>
2022-11-22 14:16:15 +0530

    now if we touch anywhere on the screen our player spaceship go to the opposite side of that touch position

--- Asteroid-Avoider (URP)/Assets/Scripts/PlayerMovement.cs
+++ Asteroid-Avoider (URP)/Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,40 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float forceMegnitude;
+    [SerializeField] float maxVelocity;
+
+    Rigidbody rb;
+    Vector3 movementDirection;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-            Debug.Log(touchPosition);
 
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(touchPosition);
-            Debug.Log(worldPosition);
+
+            movementDirection = transform.position - worldPosition;
+            movementDirection.z = 0f;
+            movementDirection.Normalize();
         }
+        else
+        {
+            movementDirection = Vector3.zero;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(movementDirection == Vector3.zero) { return; }
+
+        rb.AddForce(movementDirection * forceMegnitude, ForceMode.Force);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
     }
 }
