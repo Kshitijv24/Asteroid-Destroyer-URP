9513ffda159e35f6d98f1781f28a662d86c930a6
Kshitijv24 <kshitijv24@gmail.com>
2023-09-03 11:10:37 +0530

    Added Shield Pickup spawn Chance
    When destroying asteroids

--- Asteroid-Destroyer (URP)/Assets/Scripts/Asteroids/Asteroid.cs
+++ Asteroid-Destroyer (URP)/Assets/Scripts/Asteroids/Asteroid.cs
@@ -3,37 +3,65 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     [SerializeField] GameObject asteroidExplosionVFX;
+    [SerializeField] GameObject shieldPickUp;
+    [SerializeField] float spawnChanceOfSpawningShieldPickUp;
     [SerializeField] AudioClip asteroidDestroySFX;
-    [SerializeField] float targetFollowSpeed;
 
     private void OnTriggerEnter(Collider other)
+    {
+        HandleAsteroidCollisionWithOtherAsteroids(other);
+        HandleAsteroidCollisionWithPlayer(other);
+    }
+    private void HandleAsteroidCollisionWithOtherAsteroids(Collider other)
     {
         if (other.GetComponent<Asteroid>())
         {
             AsteroidDestroyedByOtherAsteroid();
         }
+    }
 
+    private void HandleAsteroidCollisionWithPlayer(Collider other)
+    {
         PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
 
         if (playerHealth != null)
         {
             playerHealth.DamagePlayer(1);
+            AsteroidDestroyedByPlayer();
         }
     }
 
-    public void AsteroidDestroyedByOtherAsteroid()
+    private void AsteroidDestroyedByOtherAsteroid()
     {
-        AudioManager.Instance.PlaySound(asteroidDestroySFX, 1f);
-        Instantiate(asteroidExplosionVFX, transform.position, Quaternion.identity);
-        gameObject.SetActive(false);
+        PlayEffectsAndDisableAsteroid();
     }
 
     public void AsteroidDestroyedByPlayer()
     {
-        PlayerLevelUpManager.Instance.IncrementKilledEnemies();
-        ScoreSystem.Instance.IncrementScore();
+        SpawnShieldPickUp();
+        IncrementScore();
+        PlayEffectsAndDisableAsteroid();
+    }
+
+    private void PlayEffectsAndDisableAsteroid()
+    {
         AudioManager.Instance.PlaySound(asteroidDestroySFX, 1f);
         Instantiate(asteroidExplosionVFX, transform.position, Quaternion.identity);
         gameObject.SetActive(false);
+    }
+
+    private static void IncrementScore()
+    {
+        PlayerLevelUpManager.Instance.IncrementKilledEnemies();
+        ScoreSystem.Instance.IncrementScore();
+    }
+
+    private void SpawnShieldPickUp()
+    {
+        if (Random.value < spawnChanceOfSpawningShieldPickUp)
+        {
+            Instantiate(shieldPickUp, transform.position, Quaternion.identity);
+            Debug.Log("Shield Spawned");
+        }
     }
 }
