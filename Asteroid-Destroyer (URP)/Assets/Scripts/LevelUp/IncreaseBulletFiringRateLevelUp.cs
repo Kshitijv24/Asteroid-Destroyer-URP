a99ab1fb6f34de6db3a6945458f2a21bbcf7a76f
Kshitijv24 <kshitijv24@gmail.com>
2023-09-20 12:25:54 +0530

    Added Third shooting point for the Player space ship

--- Asteroid-Destroyer (URP)/Assets/Scripts/LevelUp/IncreaseBulletFiringRateLevelUp.cs
+++ Asteroid-Destroyer (URP)/Assets/Scripts/LevelUp/IncreaseBulletFiringRateLevelUp.cs
@@ -8,8 +8,10 @@ public class IncreaseBulletFiringRateLevelUp : MonoBehaviour
     {
         float firingRateOfUp = PlayerShootingFromUp.Instance.GetBulletFireRate();
         float firingRateOfLeft = PlayerShootingFromLeft.Instance.GetBulletFireRate();
+        float firingRateOfRight = PlayerShootingFromRight.Instance.GetBulletFireRate();
         PlayerShootingFromUp.Instance.SetBulletFiringRate(firingRateOfUp - 0.1f);
         PlayerShootingFromLeft.Instance.SetBulletFiringRate(firingRateOfLeft - 0.1f);
+        PlayerShootingFromRight.Instance.SetBulletFiringRate(firingRateOfRight - 0.1f);
         GameManager.Instance.ResumeGame();
     }
 }
--- Asteroid-Destroyer (URP)/Assets/Scripts/LevelUp/IncreaseBulletSpeedLevelUp.cs
+++ Asteroid-Destroyer (URP)/Assets/Scripts/LevelUp/IncreaseBulletSpeedLevelUp.cs
@@ -8,8 +8,10 @@ public class IncreaseBulletSpeedLevelUp : MonoBehaviour
 	{
 		float bulletSpeedOfUp = PlayerShootingFromUp.Instance.GetBulletSpeed();
 		float bulletSpeedOfLeft = PlayerShootingFromLeft.Instance.GetBulletSpeed();
+		float bulletSpeedOfRight = PlayerShootingFromRight.Instance.GetBulletSpeed();
 		PlayerShootingFromUp.Instance.SetBulletSpeed(bulletSpeedOfUp + 1.0f);
 		PlayerShootingFromLeft.Instance.SetBulletSpeed(bulletSpeedOfLeft + 1.0f);
+		PlayerShootingFromRight.Instance.SetBulletSpeed(bulletSpeedOfRight + 1.0f);
 		GameManager.Instance.ResumeGame();
 	}
 }
