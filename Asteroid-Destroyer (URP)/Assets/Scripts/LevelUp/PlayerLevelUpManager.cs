1219e283c7b55800ca77efbe50257e33b8cfeeda
Kshitijv24 <kshitijv24@gmail.com>
2023-10-09 12:10:02 +0530

    fixed an mistyped

--- Asteroid-Destroyer (URP)/Assets/Scripts/LevelUp/PlayerLevelUpManager.cs
+++ Asteroid-Destroyer (URP)/Assets/Scripts/LevelUp/PlayerLevelUpManager.cs
@@ -34,7 +34,7 @@ public class PlayerLevelUpManager : MonoBehaviour
 
     public void LevelUP()
     {
-        if(killedEnemies == noOfEnemiesNeededToKillToLevelUp && PlayerHealth.Instance.GetPlayerHealth() > 1)
+        if(killedEnemies == noOfEnemiesNeededToKillToLevelUp && PlayerHealth.Instance.GetPlayerHealth() > 0)
         {
             killedEnemies = 0;
             GameManager.Instance.PauseGame();
