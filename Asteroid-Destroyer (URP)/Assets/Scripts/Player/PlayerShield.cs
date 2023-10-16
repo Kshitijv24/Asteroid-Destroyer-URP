086e92de25ca07b39a0abff67de942912f10df5c
Kshitijv24 <kshitijv24@gmail.com>
2023-10-16 21:14:32 +0530

    Changed the way that shield Behave
    instead of running a set timer on shield after which it's gets disabled or if a asteroid touches it, now there is no timer and if an asteroid touches it it's gets disabled otherwise it's last forever.

--- Asteroid-Destroyer (URP)/Assets/Scripts/Player/PlayerShield.cs
+++ Asteroid-Destroyer (URP)/Assets/Scripts/Player/PlayerShield.cs
@@ -4,20 +4,6 @@ using UnityEngine;
 
 public class PlayerShield : MonoBehaviour
 {
-    float elapsedTime = 0.0f;
-    float timeToWait = 5.0f;
-
-    private void Update()
-    {
-        elapsedTime += Time.deltaTime;
-
-        if (elapsedTime >= timeToWait)
-        {
-            elapsedTime = 0.0f;
-            gameObject.SetActive(false);
-        }
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         Asteroid asteroid = other.GetComponent<Asteroid>();
@@ -25,5 +11,6 @@ public class PlayerShield : MonoBehaviour
         if (asteroid == null) return;
 
         asteroid.AsteroidDestroyedByPlayer();
+        gameObject.SetActive(false);
     }
 }
