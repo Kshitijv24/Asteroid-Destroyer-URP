e41df10ed8b08b0f6af294ebb83a3afb438c792d
Kshitijv24 <kshitijv24@gmail.com>
2023-11-07 13:41:18 +0530

    Added backend for the Player Level Up Bar UI

--- Asteroid-Destroyer (URP)/Assets/Scripts/LevelUp/PlayerLevelUpManager.cs
+++ Asteroid-Destroyer (URP)/Assets/Scripts/LevelUp/PlayerLevelUpManager.cs
@@ -6,9 +6,10 @@ public class PlayerLevelUpManager : MonoBehaviour
 {
 	public static PlayerLevelUpManager Instance { get; private set; }
 
-    [SerializeField] int noOfEnemiesNeededToKillToLevelUp = 5;
+    public int noOfEnemiesNeededToKillToLevelUp = 5;
 
-    int killedEnemies;
+    [HideInInspector] public int killedEnemies;
+    [HideInInspector] public int playerLevel = 1;
 
     private void Awake()
     {
@@ -35,12 +36,13 @@ public class PlayerLevelUpManager : MonoBehaviour
 
     public void LevelUP()
     {
-        if(killedEnemies == noOfEnemiesNeededToKillToLevelUp 
+        if(killedEnemies >= noOfEnemiesNeededToKillToLevelUp 
             && PlayerHealth.Instance.GetPlayerHealth() > 0)
         {
+            playerLevel++;
             killedEnemies = 0;
             GameManager.Instance.PauseGameAndShowLevelUpPanel();
-            //noOfEnemiesNeededToKillToLevelUp++;
+            noOfEnemiesNeededToKillToLevelUp++;
         }
     }
 }
--- Asteroid-Destroyer (URP)/Assets/Scripts/Player/PlayerLevelUpBar.cs
+++ Asteroid-Destroyer (URP)/Assets/Scripts/Player/PlayerLevelUpBar.cs
@@ -1,8 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerLevelUpBar : MonoBehaviour
 {
-	
+	[SerializeField] Slider playerLevelUpBarSlider;
+    [SerializeField] TextMeshProUGUI playerLevelText;
+
+    
+
+    private void Start()
+    {
+        playerLevelUpBarSlider.value = 0;
+    }
+
+    private void Update()
+    {
+        playerLevelUpBarSlider.maxValue = PlayerLevelUpManager.Instance.noOfEnemiesNeededToKillToLevelUp;
+        playerLevelUpBarSlider.value = PlayerLevelUpManager.Instance.killedEnemies;
+
+        playerLevelText.text = "Player Level " + PlayerLevelUpManager.Instance.playerLevel.ToString();
+    }
 }
