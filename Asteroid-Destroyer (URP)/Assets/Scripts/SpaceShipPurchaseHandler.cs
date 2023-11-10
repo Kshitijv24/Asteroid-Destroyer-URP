25aeda334af6591f9919eaa7a28bdd2f8ac090d5
Kshitijv24 <kshitijv24@gmail.com>
2023-11-10 11:46:03 +0530

    Separated the logic of One Script into Two Scripts
    Separated Ship Selection logic and Ship Purchase logic To sperate scripts.

---
+++ Asteroid-Destroyer (URP)/Assets/Scripts/SpaceShipPurchaseHandler.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpaceShipPurchaseHandler : MonoBehaviour
+{
+    [SerializeField] SpaceShipSelection spaceShipSelection;
+    [SerializeField] SpaceShip[] spaceShipArray;
+    [SerializeField] Button buyButton;
+    [SerializeField] Button selectButton;
+    [SerializeField] Image lockedSpaceShipImage;
+
+    private void Start()
+    {
+        CheckIfSpaceShipIsUnlocked();
+    }
+
+    private void Update()
+    {
+        HandleSpaceShipPurchase();
+    }
+
+    private void CheckIfSpaceShipIsUnlocked()
+    {
+        foreach (SpaceShip spaceShip in spaceShipArray)
+        {
+            if (spaceShip.price == 0)
+            {
+                spaceShip.isUnlocked = true;
+            }
+            else
+            {
+                spaceShip.isUnlocked = PlayerPrefs.GetInt(spaceShip.name) == 0 ? false : true;
+            }
+        }
+    }
+
+    public void BuySpaceShip()
+    {
+        SpaceShip spaceShip = spaceShipArray[spaceShipSelection.GetSelectedSpaceShip()];
+        PlayerPrefs.SetInt(spaceShip.name, 1);
+
+        spaceShip.isUnlocked = true;
+        PlayerPoints.Instance.DecreasePlayerPoints(spaceShip.price);
+    }
+
+    private void ShowUnlockSpaceShipUI()
+    {
+        buyButton.gameObject.SetActive(false);
+        lockedSpaceShipImage.gameObject.SetActive(false);
+        selectButton.gameObject.SetActive(true);
+    }
+
+    private void ShowLockSpaceShipUI()
+    {
+        buyButton.gameObject.SetActive(true);
+        selectButton.gameObject.SetActive(false);
+        lockedSpaceShipImage.gameObject.SetActive(true);
+    }
+
+    private void HandleSpaceShipPurchase()
+    {
+        SpaceShip spaceShip = spaceShipArray[spaceShipSelection.GetSelectedSpaceShip()];
+
+        if (spaceShip.isUnlocked)
+        {
+            ShowUnlockSpaceShipUI();
+        }
+        else
+        {
+            ShowLockSpaceShipUI();
+            ShowPriceOfSpaceShipInBuyButton(spaceShip);
+            BuySpaceShipIfHaveEnoughMoney(spaceShip);
+        }
+    }
+
+    private void ShowPriceOfSpaceShipInBuyButton(SpaceShip spaceShip)
+    {
+        buyButton.GetComponentInChildren<TextMeshProUGUI>().text = "Buy: " + spaceShip.price;
+    }
+
+    private void BuySpaceShipIfHaveEnoughMoney(SpaceShip spaceShip)
+    {
+        buyButton.interactable = spaceShip.price <= PlayerPoints.Instance.GetPlayerPoints();
+    }
+}
--- Asteroid-Destroyer (URP)/Assets/Scripts/SpaceShipSelection.cs
+++ Asteroid-Destroyer (URP)/Assets/Scripts/SpaceShipSelection.cs
@@ -9,16 +9,11 @@ public class SpaceShipSelection : MonoBehaviour
 {
 	[SerializeField] List<GameObject> allPlayerSpaceShipList = new List<GameObject>();
     [SerializeField] float spaceShipRotationSpeed = 50.0f;
-    [SerializeField] SpaceShip[] spaceShipArray;
-    [SerializeField] Button buyButton;
-    [SerializeField] Button selectButton;
-    [SerializeField] Image lockedSpaceShipImage;
 
     int selectedSpaceShip = 0;
 
     private void Start()
     {
-        CheckIfSpaceShipIsUnlocked();
         OnlyShowingFirstSpaceShipAtTheStart();
     }
 
@@ -32,25 +27,9 @@ public class SpaceShipSelection : MonoBehaviour
         allPlayerSpaceShipList[0].SetActive(true);
     }
 
-    private void CheckIfSpaceShipIsUnlocked()
-    {
-        foreach (SpaceShip spaceShip in spaceShipArray)
-        {
-            if (spaceShip.price == 0)
-            {
-                spaceShip.isUnlocked = true;
-            }
-            else
-            {
-                spaceShip.isUnlocked = PlayerPrefs.GetInt(spaceShip.name) == 0 ? false : true;
-            }
-        }
-    }
-
     private void Update()
     {
         RotateSpaceShips();
-        HandleSpaceShipPurchase();
     }
 
     private void RotateSpaceShips()
@@ -84,52 +63,8 @@ public class SpaceShipSelection : MonoBehaviour
         SceneManager.LoadScene(2);
     }
 
-    public void BuySpaceShip()
+    public int GetSelectedSpaceShip()
     {
-        SpaceShip spaceShip = spaceShipArray[selectedSpaceShip];
-        PlayerPrefs.SetInt(spaceShip.name, 1);
-
-        spaceShip.isUnlocked = true;
-        PlayerPoints.Instance.DecreasePlayerPoints(spaceShip.price);
-    }
-
-    private void ShowUnlockSpaceShipUI()
-    {
-        buyButton.gameObject.SetActive(false);
-        lockedSpaceShipImage.gameObject.SetActive(false);
-        selectButton.gameObject.SetActive(true);
-    }
-
-    private void ShowLockSpaceShipUI()
-    {
-        buyButton.gameObject.SetActive(true);
-        selectButton.gameObject.SetActive(false);
-        lockedSpaceShipImage.gameObject.SetActive(true);
-    }
-
-    private void HandleSpaceShipPurchase()
-    {
-        SpaceShip spaceShip = spaceShipArray[selectedSpaceShip];
-
-        if(spaceShip.isUnlocked)
-        {
-            ShowUnlockSpaceShipUI();
-        }
-        else
-        {
-            ShowLockSpaceShipUI();
-            ShowPriceOfSpaceShipInBuyButton(spaceShip);
-            BuySpaceShipIfHaveEnoughMoney(spaceShip);
-        }
-    }
-
-    private void ShowPriceOfSpaceShipInBuyButton(SpaceShip spaceShip)
-    {
-        buyButton.GetComponentInChildren<TextMeshProUGUI>().text = "Buy: " + spaceShip.price;
-    }
-
-    private void BuySpaceShipIfHaveEnoughMoney(SpaceShip spaceShip)
-    {
-        buyButton.interactable = spaceShip.price <= PlayerPoints.Instance.GetPlayerPoints();
+        return selectedSpaceShip;
     }
 }
