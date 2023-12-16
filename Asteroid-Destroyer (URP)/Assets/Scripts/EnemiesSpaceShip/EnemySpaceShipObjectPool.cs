d1b59ae3489c038c43333b5f064480ab5cbe3ec9
Kshitijv24 <kshitijv24@gmail.com>
2023-12-16 11:07:12 +0530

    Added Enemy Space Ship Object Pool

---
+++ Asteroid-Destroyer (URP)/Assets/Scripts/EnemiesSpaceShip/EnemySpaceShipObjectPool.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpaceShipObjectPool : MonoBehaviour
+{
+    public static EnemySpaceShipObjectPool Instance { get; private set; }
+
+    [SerializeField] GameObject[] enemySpaceShipPrefabArray;
+    [SerializeField] int amountToPool = 10;
+
+    List<GameObject> pooledGameObjectList = new List<GameObject>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject instantiatedGameObject =
+                Instantiate(enemySpaceShipPrefabArray[Random.Range(0, enemySpaceShipPrefabArray.Length)], transform);
+
+            instantiatedGameObject.SetActive(false);
+            pooledGameObjectList.Add(instantiatedGameObject);
+        }
+    }
+
+    public GameObject GetPooledGameObject()
+    {
+        for (int i = 0; i < pooledGameObjectList.Count; i++)
+        {
+            if (!pooledGameObjectList[i].activeInHierarchy)
+            {
+                return pooledGameObjectList[i];
+            }
+        }
+        return null;
+    }
+}
---
+++ Asteroid-Destroyer (URP)/Assets/Scripts/EnemiesSpaceShip/EnemySpaceShipSpawner.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpaceShipSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] enemySpaceShipPrefab;
+    [SerializeField] float spawnTime = 1;
+    [SerializeField] Vector2 forceRange;
+
+    float timer;
+    Camera mainCamera;
+    Rigidbody rb;
+
+    private void Awake() => mainCamera = Camera.main;
+
+    private void Update() => SpawnEnemySpaceShipAtATime();
+
+    private void SpawnEnemySpaceShipAtATime()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            SpawnEnemySpaceShip();
+            timer += spawnTime;
+
+            ChangeEnemySpaceShipSpawnRateAccordingToThePlayerLevel();
+        }
+    }
+
+    private void SpawnEnemySpaceShip()
+    {
+        int side = Random.Range(0, 4);
+
+        Vector2 spawnPoint = Vector2.zero;
+        Vector2 direction = Vector2.zero;
+
+        switch (side)
+        {
+            case 0:
+                spawnPoint.x = 0;
+                spawnPoint.y = Random.value;
+                direction = new Vector2(1f, Random.Range(-1f, 1f));
+                break;
+            case 1:
+                spawnPoint.x = 1;
+                spawnPoint.y = Random.value;
+                direction = new Vector2(-1f, Random.Range(-1f, 1f));
+                break;
+            case 2:
+                spawnPoint.x = Random.value;
+                spawnPoint.y = 0;
+                direction = new Vector2(Random.Range(-1f, 1f), 1f);
+                break;
+            case 3:
+                spawnPoint.x = Random.value;
+                spawnPoint.y = 1;
+                direction = new Vector2(Random.Range(-1f, 1f), -1f);
+                break;
+        }
+
+        Vector3 worldSpawnPoint = mainCamera.ViewportToWorldPoint(spawnPoint);
+        worldSpawnPoint.z = 0;
+
+        GameObject pooledAsteroidsPrefab = AsteroidsObjectPool.Instance.GetPooledGameObject();
+
+        if (pooledAsteroidsPrefab != null)
+        {
+            pooledAsteroidsPrefab.transform.position = worldSpawnPoint;
+            pooledAsteroidsPrefab.transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+            pooledAsteroidsPrefab.SetActive(true);
+            rb = pooledAsteroidsPrefab.GetComponent<Rigidbody>();
+        }
+
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.velocity = direction.normalized * Random.Range(forceRange.x, forceRange.y);
+        }
+    }
+
+    private void ChangeEnemySpaceShipSpawnRateAccordingToThePlayerLevel()
+    {
+        if (PlayerLevelUpManager.Instance.playerLevel == 2)
+        {
+            spawnTime = 0.8f;
+        }
+        else if (PlayerLevelUpManager.Instance.playerLevel == 4)
+        {
+            spawnTime = 0.6f;
+        }
+        else if (PlayerLevelUpManager.Instance.playerLevel == 6)
+        {
+            spawnTime = 0.4f;
+        }
+    }
+}
