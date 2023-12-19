4a270a112bbff0ef4869c0d03d7403bce33b0b4d
Kshitijv24 <kshitijv24@gmail.com>
2023-12-19 14:06:58 +0530

    Fixed an issue
    of shield not getting disabled after colliding with the enemy space ship.

--- Asteroid-Destroyer (URP)/Assets/Scripts/Player/PlayerShield.cs
+++ Asteroid-Destroyer (URP)/Assets/Scripts/Player/PlayerShield.cs
@@ -7,10 +7,18 @@ public class PlayerShield : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         AsteroidCollision asteroid = other.GetComponent<AsteroidCollision>();
+        EnemySpaceShipCollision enemySpaceShipCollision = other.GetComponent<EnemySpaceShipCollision>();
 
-        if (asteroid == null) return;
+        if (asteroid != null)
+        {
+            asteroid.AsteroidDestroyedByPlayer();
+            gameObject.SetActive(false);
+        }
 
-        asteroid.AsteroidDestroyedByPlayer();
-        gameObject.SetActive(false);
+        if (enemySpaceShipCollision != null)
+        {
+            enemySpaceShipCollision.EnemySpaceShipDestroyedByPlayer();
+            gameObject.SetActive(false);
+        }
     }
 }
